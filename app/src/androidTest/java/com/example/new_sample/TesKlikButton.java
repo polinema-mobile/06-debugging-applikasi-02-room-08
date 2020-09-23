5213dfce5117f5eb28d209cc96b9f1e24f37121c
RioCyber936 <adirio205@gmail.com>
2020-09-23 21:21:09 +0700

    Testing operasi dan button angka

--- app/src/androidTest/java/com/example/new_sample/TesKlikButton.java
+++ app/src/androidTest/java/com/example/new_sample/TesKlikButton.java
@@ -21,10 +21,55 @@ public class TesKlikButton {
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    public void tesKlikButtonSatu(){
+    public void tesKlikAngka(){
         //cari id button1 terus di klik
         onView(withId(R.id.button1)).perform(click());
-        onView(withId(R.id.input)).check(matches(withText("1")));
+        onView(withId(R.id.button2)).perform(click());
+        onView(withId(R.id.button3)).perform(click());
+        onView(withId(R.id.button4)).perform(click());
+        onView(withId(R.id.button5)).perform(click());
+        onView(withId(R.id.button6)).perform(click());
+        onView(withId(R.id.button7)).perform(click());
+        onView(withId(R.id.button8)).perform(click());
+        onView(withId(R.id.button9)).perform(click());
+        onView(withId(R.id.button0)).perform(click());
+        onView(withId(R.id.input)).check(matches(withText("1234567890")));
+    }
+
+    public void penjumlahan(){
+        //cari id button1 terus di klik
+        onView(withId(R.id.button9)).perform(click());
+        onView(withId(R.id.button_add)).perform(click());
+        onView(withId(R.id.button2)).perform(click());
+        onView(withId(R.id.button_equal)).perform(click());
+        onView(withId(R.id.input)).check(matches(withText("11")));
+    }
+
+    public void pengurangan(){
+        //cari id button1 terus di klik
+        onView(withId(R.id.button9)).perform(click());
+        onView(withId(R.id.button_sub)).perform(click());
+        onView(withId(R.id.button2)).perform(click());
+        onView(withId(R.id.button_equal)).perform(click());
+        onView(withId(R.id.input)).check(matches(withText("7")));
+    }
+
+    public void perkalian(){
+        //cari id button1 terus di klik
+        onView(withId(R.id.button9)).perform(click());
+        onView(withId(R.id.button_multi)).perform(click());
+        onView(withId(R.id.button2)).perform(click());
+        onView(withId(R.id.button_equal)).perform(click());
+        onView(withId(R.id.input)).check(matches(withText("18")));
+    }
+
+    public void pembagian(){
+        //cari id button1 terus di klik
+        onView(withId(R.id.button9)).perform(click());
+        onView(withId(R.id.button_sub)).perform(click());
+        onView(withId(R.id.button3)).perform(click());
+        onView(withId(R.id.button_equal)).perform(click());
+        onView(withId(R.id.input)).check(matches(withText("3")));
     }
 
 }
